08e0775a648ef0036f5268e9f8837645f641764b
Sami Nasry <skelowthes@gmail.com>
2025-02-16 02:46:30 +0100

    Progress

--- Javascript_Practice/Library/script.js
+++ Javascript_Practice/Library/script.js
@@ -10,4 +10,30 @@ function book(title, author, pages)
 const harry = new book("Harry", 'Sami' , 500);
 const book2 = new book("random", "ran" , 10);
 
-The_Library.push(harry, book2);
+The_Library.push(harry);
+The_Library.push(book2);
+
+function displayBooks() {
+    const table = document.querySelector("table");
+
+    The_Library.forEach(book => {
+        let row = document.createElement("tr");
+
+        let titleCell = document.createElement("td");
+        titleCell.textContent = book.title;
+
+        let authorCell = document.createElement("td");
+        authorCell.textContent = book.author;
+
+        let pagesCell = document.createElement("td");
+        pagesCell.textContent = book.pages;
+
+        row.appendChild(titleCell);
+        row.appendChild(authorCell);
+        row.appendChild(pagesCell);
+
+        table.appendChild(row);
+    });
+}
+
+displayBooks();
