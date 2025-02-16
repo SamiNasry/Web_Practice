2ed8889016882a074917dead846de322f18638be
Sami Nasry <skelowthes@gmail.com>
2025-02-16 02:56:50 +0100

    Project done my add make it better later

--- Javascript_Practice/Library/script.js
+++ Javascript_Practice/Library/script.js
@@ -16,6 +16,11 @@ The_Library.push(book2);
 function displayBooks() {
     const table = document.querySelector("table");
 
+    const rows = document.getElementsByTagName("tr");
+    while(rows.length > 1) {
+        table.deleteRow(1);
+    }
+
     The_Library.forEach(book => {
         let row = document.createElement("tr");
 
@@ -36,4 +41,21 @@ function displayBooks() {
     });
 }
 
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+
+    const newBook = new book(title, author, parseInt(pages));
+    The_Library.push(newBook);
+    displayBooks();
+    form.reset();
+});
+
 displayBooks();
+
